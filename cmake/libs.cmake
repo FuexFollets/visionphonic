8c079860d1e8a7523e00884849cf186996bbe6a8
FuexFollets <fuex9follets@gmail.com>
2023-09-30 12:43:34 -0400

    Testing methods of waveform extraction from audio files

--- cmake/libs.cmake
+++ cmake/libs.cmake
@@ -1,1 +1,2 @@
 add_subdirectory(${PROJECT_SOURCE_DIR}/lib/SFML)
+# add_subdirectory(${PROJECT_SOURCE_DIR}/lib/AudioFile)
--- tests/test_sfml_audio.cpp
+++ tests/test_sfml_audio.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <iterator>
 #include <SFML/Audio.hpp>
 
-int main() {
+int main(int argc, char** argv) {
+    sf::SoundBuffer buffer;
+
+    if (argc < 2) {
+        std::cout << "Usage: " << *std::next(argv, 1) << " <filename>\n";
+        return 1;
+    }
+
+    if (!buffer.loadFromFile(*std::next(argv, 1))) {
+        std::cout << "Error loading file\n";
+        return 1;
+    }
+
+    std::cout << "First sample: " << *buffer.getSamples() << "\n";
+    std::cout << "Channel count: " << buffer.getChannelCount() << "\n";
     std::cout << "Printing from test_sfml_audio.cpp\n";
+
+    const bool successful {buffer.loadFromSamples(buffer.getSamples(), 1 / 2 * buffer.getSampleCount(),
+                                                  1, buffer.getSampleRate() * 4)};
+
+    std::cout << "Successful: " << successful << "\n";
+
+    sf::Sound sound {buffer};
+
+    sound.play();
+
+    while (sound.getStatus() == sf::Sound::Playing) {
+        sf::sleep(sf::milliseconds(100));
+    }
 }
